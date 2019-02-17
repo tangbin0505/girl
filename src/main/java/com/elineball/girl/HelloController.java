12e3884cc1782a3251fcdbce7207c644b96cc0fe
tangbin0505 <464403694@qq.com>
2019-02-17 11:01:16 +0800

    数据库操作上

---
+++ src/main/java/com/elineball/girl/HelloController.java
@@ -0,0 +1,24 @@
+package com.elineball.girl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/hello",method = RequestMethod.GET)
+public class HelloController {
+
+    @Autowired
+    private GirlProperties girlProperties;
+
+    @GetMapping(value = "/say/{id}")
+    public  String say(@PathVariable("id") Integer myid){
+      //  return girlProperties.getCupSize();
+        return "id : " + myid;
+    }
+    @GetMapping(value = "/age")
+    public  String age(@RequestParam(value = "id",required = false,defaultValue = "0") Integer id){
+        //return girlProperties.getAge();
+        return "id : " + id;
+    }
+}
